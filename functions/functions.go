c8b8bc4bb2d8bb548836aec685ad3eeca4e3ea39
Rafael Patino <rafael.patino@mercadolibre.com.co>
2022-02-02 14:05:47 -0500

    Funciones anonimas

---
+++ functions/functions.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	x := 5
+	//Funcion anonima
+	y := func() int {
+		return x + 2
+	}() //Los parentesis al final es para indicar la llamada la funcion anonima
+	fmt.Println(y)
+
+	c := make(chan int)
+	go func() {
+		fmt.Println("Starting function")
+		time.Sleep(2 * time.Second)
+		fmt.Println("End")
+		c <- 1
+	}()
+	<-c
+}
